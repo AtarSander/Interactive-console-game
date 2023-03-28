f3d90b2c8f6ceb436640ad567a7788e58089dc10
root <aszymczy>
2023-03-28 18:11:37 +0200

    Created error handling

--- LAB2/chapter.cpp
+++ LAB2/chapter.cpp
@@ -1,5 +1,6 @@
 #include "chapter.hpp"
 #include <string>
+#include <stdexcept>
 
 std::string Chapter::get_story() const { return _story; }
 
@@ -8,10 +9,7 @@ unsigned int Chapter::get_seed() const { return _seed; }
 unsigned int Chapter::outcome(std::string response)
 {
     auto it = _list_of_options.find(response);
-    if (it != _list_of_options.end()) return it->second;
-    else
-    {
-        return 0;
-    }
+    if (it != _list_of_options.end()) { return it->second; }
+    else { throw std::invalid_argument("Wrong input: " + response); }
 
 }

59d4e84f6be5fed07552f3a1004baa50a6693579
root <aszymczy>
2023-03-28 21:19:51 +0200

    Created read_from_file method

--- LAB2/chapter.cpp
+++ LAB2/chapter.cpp
@@ -1,5 +1,7 @@
 #include "chapter.hpp"
 #include <string>
+#include <fstream>
+#include <sstream>
 #include <stdexcept>
 
 std::string Chapter::get_story() const { return _story; }
@@ -11,5 +13,42 @@ unsigned int Chapter::outcome(std::string response)
     auto it = _list_of_options.find(response);
     if (it != _list_of_options.end()) { return it->second; }
     else { throw std::invalid_argument("Wrong input: " + response); }
+
+}
+
+void Chapter::read_from_file(std::string filename)
+{
+    std::ifstream file(filename);
+
+    if (!file.is_open())
+    {
+        throw std::runtime_error("Couldn't open the file.");
+    }
+
+
+    file >> _seed;
+    std::string line;
+    while (std::getline(file, line))
+    {
+        if (line == "RESPONSE")
+        {
+            break;
+        }
+        _story += line + "\n";
+    }
+
+    while (std::getline(file, line))
+    {
+        if (line.empty())
+        {
+            break;
+        }
+        std::istringstream iss(line);
+        std::string option_text;
+        int option_number;
+        iss >> option_text >> option_number;
+        _list_of_options[option_text] = option_number;
+    }
+    file.close();
 
 }
--- LAB2/chapter.hpp
+++ LAB2/chapter.hpp
@@ -1,3 +1,5 @@
+#ifndef CHAPTER_HPP
+#define CHAPTER_HPP
 #include <string>
 #include <list>
 #include <map>
@@ -5,12 +7,15 @@
 class Chapter
 {
     public:
+        Chapter() = default;
         Chapter(unsigned int seed) : _seed(seed){};
         std::string get_story() const;
         unsigned int get_seed() const;
         unsigned int outcome(std::string response);
+        void read_from_file(std::string filename);
     private:
         std::string _story;
         std::map<std::string, unsigned int> _list_of_options;
         unsigned int _seed;
 };
+#endif

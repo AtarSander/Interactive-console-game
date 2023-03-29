3d80ffebd4f9318c25c40a92fc8d01863c1b2bf6
root <aszymczy>
2023-03-29 09:22:11 +0200

    Final main example

--- LAB2/main.cpp
+++ LAB2/main.cpp
@@ -14,19 +14,24 @@ int main(int argc, char *argv[])
     std::string name = argv[1];
     int health = std::stoi(argv[2]);
     Player player(name, health);
+
+
     Game game1(player);
-    Chapter chapter1(0);
-    chapter1.read_from_file("seed_0.txt");
-    Chapter chapter2(1);
-    chapter2.read_from_file("seed_1.txt");
-    Chapter chapter3(2);
-    chapter3.read_from_file("seed_2.txt");
-    Chapter chapter4(3);
-    chapter4.read_from_file("seed_3.txt");
-    Chapter chapter5(4);
-    chapter5.read_from_file("seed_4.txt");
-    Chapter chapter6(5);
-    chapter6.read_from_file("seed_5.txt");
+    Chapter chapter0(0);
+    chapter0.read_from_file("seed_0.txt");
+    Chapter chapter1(1);
+    chapter1.read_from_file("seed_1.txt");
+    Chapter chapter2(2);
+    chapter2.read_from_file("seed_2.txt");
+    Chapter chapter3(3);
+    chapter3.read_from_file("seed_3.txt");
+    Chapter chapter4(4);
+    chapter4.read_from_file("seed_4.txt");
+    Chapter chapter5(5);
+    chapter5.read_from_file("seed_5.txt");
+    Chapter chapter6(6);
+    chapter6.read_from_file("seed_6.txt");
+    game1.add_chapter(chapter0);
     game1.add_chapter(chapter1);
     game1.add_chapter(chapter2);
     game1.add_chapter(chapter3);
@@ -34,7 +39,6 @@ int main(int argc, char *argv[])
     game1.add_chapter(chapter5);
     game1.add_chapter(chapter6);
     game1.play();
-
 
     return 0;
 }

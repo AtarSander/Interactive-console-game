7e11c51d76e876c69073ae772c73f509889f9330
root <aszymczy>
2023-03-29 08:30:11 +0200

    Finished test for chapter class

--- LAB2/test/test_chapter.cpp
+++ LAB2/test/test_chapter.cpp
@@ -4,34 +4,38 @@
 
 TEST_CASE("Chapter constructor and getter functions")
 {
+    SECTION("Default chapter gets correct parameters")
+    {
     Chapter chapter;
     REQUIRE(chapter.get_story() == "");
     REQUIRE(chapter.get_seed() == 0);
+    }
 
+    SECTION("Consctructor gives correct properities")
+    {
     Chapter chapter2(123);
     REQUIRE(chapter2.get_story() == "");
     REQUIRE(chapter2.get_seed() == 123);
+    }
 }
 
 TEST_CASE("Chapter outcome function")
 {
     Chapter chapter(123);
-    chapter.read_from_file("../seed_0.txt");
+    chapter.read_from_file("seed_0.txt");
 
     REQUIRE(chapter.outcome("light the torch") == 1);
     REQUIRE(chapter.outcome("turn back") == 2);
-
-    REQUIRE_THROWS_WITH(chapter.outcome("search for another path"), "Wrong input: search for another path");
 }
 
 TEST_CASE("Chapter read_from_file method")
 {
     Chapter chapter;
-    chapter.read_from_file("../seed_0.txt");
+    chapter.read_from_file("seed_0.txt");
 
     SECTION("Story is read correctly")
     {
-        REQUIRE(chapter.get_story() == "Welcome adventurer, to a world filled with magic and mystery. You are a treasure hunter, seeking fortune and glory in the far corners of this fantastical land. The journey ahead will be perilous, but the rewards could be great. Are you ready to embark on this adventure?\n\nYour quest begins at the entrance of a dark and foreboding cave. Legend has it that deep within its depths lies a treasure beyond your wildest dreams. As you take your first steps into the cave, the air grows colder and the darkness thickens. You can hear the sound of dripping water echoing off the walls.\n");
+        REQUIRE(chapter.get_story() == "Welcome adventurer, to a world filled with magic and mystery. You are a treasure hunter, seeking fortune and glory in the far corners of this fantastical land. The journey ahead will be perilous, but the rewards could be great. Are you ready to embark on this adventure?\r\n\r\nYour quest begins at the entrance of a dark and foreboding cave. Legend has it that deep within its depths lies a treasure beyond your wildest dreams. As you take your first steps into the cave, the air grows colder and the darkness thickens. You can hear the sound of dripping water echoing off the walls.\r\n\r\nDo you light the torch and venture forth into the darkness, or do you turn back and search for another path?\r\n");
     }
 
     SECTION("Seed is read correctly")

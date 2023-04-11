ee592930e2707d1d6d6c8f5e1738d00b7e69ed14
root <aszymczy>
2023-04-12 01:36:38 +0200

    Created items functionality

--- LAB3/player.cpp
+++ LAB3/player.cpp
@@ -1,9 +1,14 @@
 #include "player.hpp"
+#include "item.hpp"
 #include <stdexcept>
+#include <list>
+#include <algorithm>
 
 std::string Player::get_name() const { return name; }
 
 unsigned int Player::get_health() const { return health; }
+
+void Player::set_name(std::string new_name) { name = new_name;}
 
 void Player::set_health(int new_health)
 {
@@ -15,4 +20,24 @@ void Player::set_health(int new_health)
     {
         throw(std::invalid_argument("Invalid input: " + std::to_string(new_health)));
     }
+}
+
+void Player::add_item(Item new_item){  equipment.push_back(new_item);  }
+
+unsigned int Player::find_useful_item(std::string type)
+{
+    auto it = std::find_if(equipment.begin(), equipment.end(), [&](const Item& item)
+    {
+        return item.get_type() == type;
+    });
+    if (it != equipment.end())
+    {
+        --(*it);
+        if ((*it).check_uses())
+        {
+            equipment.erase(it);
+        }
+        return 0;
+    }
+    return 1;
 }

7318fd92b079bf5fecbc5cdf939fbcc9b112599e
root <aszymczy>
2023-04-12 07:47:20 +0200

    Updated tests for new functionalities

--- LAB3/test/test_player.cpp
+++ LAB3/test/test_player.cpp
@@ -29,3 +29,82 @@ TEST_CASE("Player class tests")
         REQUIRE_THROWS_AS(player.set_health(-50), std::invalid_argument);
     }
 }
+
+TEST_CASE("Player add_item method")
+{
+    Player player("John", 100);
+
+    SECTION("Adding an item to empty equipment")
+    {
+        Item item("Sword", "Weapon", 10);
+        player.add_item(item);
+
+        REQUIRE(player.get_equipment().size() == 1);
+        REQUIRE(player.get_equipment().front().get_name() == "Sword");
+        REQUIRE(player.get_equipment().front().get_type() == "Weapon");
+        REQUIRE(player.get_equipment().front().get_uses() == 10);
+    }
+
+    SECTION("Adding an item to non-empty equipment")
+    {
+        Item item1("Sword", "Weapon", 10);
+        Item item2("Potion", "Consumable", 1);
+        player.add_item(item1);
+        player.add_item(item2);
+
+        REQUIRE(player.get_equipment().size() == 2);
+        REQUIRE(player.get_equipment().front().get_name() == "Sword");
+        REQUIRE(player.get_equipment().back().get_name() == "Potion");
+    }
+}
+
+TEST_CASE("Player find_useful_item method")
+{
+    Player player("John", 100);
+
+    SECTION("Finding a useful item")
+    {
+        Item item1("Sword", "Weapon", 10);
+        Item item2("Potion", "Consumable", 1);
+        player.add_item(item1);
+        player.add_item(item2);
+
+        unsigned int result = player.find_useful_item("Consumable");
+        REQUIRE(result == 0);
+        REQUIRE(player.get_equipment().size() == 1);
+        REQUIRE(player.get_equipment().front().get_name() == "Sword");
+    }
+
+    SECTION("Not finding a useful item")
+    {
+        Item item("Sword", "Weapon", 10);
+        player.add_item(item);
+
+        unsigned int result = player.find_useful_item("Consumable");
+        REQUIRE(result == 1);
+        REQUIRE(player.get_equipment().size() == 1);
+        REQUIRE(player.get_equipment().front().get_name() == "Sword");
+    }
+
+    SECTION("Using up the last use of an item")
+    {
+        Item item("Potion", "Consumable", 1);
+        player.add_item(item);
+
+        unsigned int result = player.find_useful_item("Consumable");
+        REQUIRE(result == 0);
+        REQUIRE(player.get_equipment().empty());
+    }
+
+    SECTION("Using up a use of an item")
+    {
+        Item item("Potion", "Consumable", 2);
+        player.add_item(item);
+
+        unsigned int result = player.find_useful_item("Consumable");
+        REQUIRE(result == 0);
+        REQUIRE(player.get_equipment().size() == 1);
+        REQUIRE(player.get_equipment().front().get_name() == "Potion");
+        REQUIRE(player.get_equipment().front().get_uses() == 1);
+    }
+}

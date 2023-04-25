992717497896c74c0a2008168c1a2c793cd9c091
root <aszymczy>
2023-04-25 18:41:50 +0200

    Added getters, setters, implemented parameter transfer

--- LAB4/entity.cpp
+++ LAB4/entity.cpp
@@ -11,10 +11,11 @@ void Entity::attack(std::shared_ptr<Entity> other)
 
 void Entity::defend(int damage)
 {
-    if (base_armor < 0)
-    { base_armor = 0; }
-    base_armor = base_armor - (damage * 0.2);
-    damage = damage * 0.8;
+    if (base_armor > 0)
+    {
+        base_armor = base_armor - (damage * 0.2);
+        damage = damage * 0.8;
+    }
     health = health - damage;
 
 }
@@ -24,10 +25,26 @@ bool Entity::exists() const
     return health > 0;
 }
 
+void Entity::setHealth(int new_health)
+{
+    health = new_health;
+}
+
+void Entity::setArmor(int new_armor)
+{
+    base_armor = new_armor;
+}
+
+void Entity::transferParameter(std::string data){ parameter = data; }
+
 std::string Entity::getName() const { return name;}
+
+std::string Entity::getParameter() const { return parameter;}
 
 int Entity::getHealth() const { return health;}
 
 int Entity::getBaseDamage() const { return base_damage;}
 
 int Entity::getBaseArmor() const { return base_armor;}
+
+
--- LAB4/entity.hpp
+++ LAB4/entity.hpp
@@ -9,12 +9,16 @@ class Entity
     public:
     Entity(std::string name, int health, int base_damage, int base_armor) :
     name(name), health(health), base_damage(base_damage), base_armor(base_armor){};
-    virtual ~Entity();
+    virtual ~Entity() = default;
     virtual void attack(std::shared_ptr<Entity> other);
     virtual void defend(int damage);
     virtual bool exists() const;
+    void setHealth(int new_health);
+    void setArmor(int new_armor);
+    void transferParameter(std::string data);
 
     std::string getName() const;
+    std::string getParameter() const;
     int getHealth() const;
     int getBaseDamage() const;
     int getBaseArmor() const;
@@ -24,6 +28,7 @@ class Entity
         int health;
         int base_damage;
         int base_armor;
+        std::string parameter = "";
 };
 
 #endif

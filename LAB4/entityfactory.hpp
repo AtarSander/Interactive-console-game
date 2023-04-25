225410f16ede6ae42d2ff3bd052163b356d86458
root <aszymczy>
2023-04-25 17:10:36 +0200

    Created entityfactory header file to implement factory design pattern

---
+++ LAB4/entityfactory.hpp
@@ -0,0 +1,27 @@
+#ifndef ENTITYFACTORY_HPP
+#define ENTITYFACTORY_HPP
+#include "entity.hpp"
+#include "enemy.hpp"
+#include "weapon.hpp"
+#include <memory>
+#include <string>
+
+enum class EntityType
+{
+    Enemy,
+    Weapon
+};
+
+class EntityFactory {
+public:
+    std::unique_ptr<Entity> createEntity(EntityType type, std::string name,
+                                         int health, int base_damage,
+                                         int base_armor, std::string weapon_type,
+                                         std::map<std::string, double> enemy_resistances);
+
+private:
+    std::unique_ptr<Entity> createEnemy(std::string name, unsigned int health,
+                                        unsigned int base_damage, unsigned int base_armor);
+    std::unique_ptr<Entity> createWeapon(std::string name, unsigned int base_damage, std::string type);
+};
+#endif

68717d25ae769458908a34a0ac2454906dc560af
root <aszymczy>
2023-04-26 08:51:37 +0200

    Updated test case names

--- LAB4/tests/test_entity.cpp
+++ LAB4/tests/test_entity.cpp
@@ -3,7 +3,8 @@
 #include "catch.hpp"
 #include "../entity.hpp"
 
-TEST_CASE("Test Entity attack method") {
+TEST_CASE("Test Entity attack method")
+{
     std::shared_ptr<Entity> entity1 = std::make_shared<Entity>("Entity 1", 100, 10, 20);
     std::shared_ptr<Entity> entity2 = std::make_shared<Entity>("Entity 2", 80, 5, 10);
 
@@ -12,7 +13,8 @@ TEST_CASE("Test Entity attack method") {
     REQUIRE(entity2->getHealth() == 72);
 }
 
-TEST_CASE("Test Entity defend method with armor") {
+TEST_CASE("Test Entity defend method with armor")
+{
     std::shared_ptr<Entity> entity = std::make_shared<Entity>("Entity", 100, 10, 20);
 
     entity->defend(10);
@@ -21,7 +23,8 @@ TEST_CASE("Test Entity defend method with armor") {
     REQUIRE(entity->getHealth() == 92);
 }
 
-TEST_CASE("Test Entity defend method without armor") {
+TEST_CASE("Test Entity defend method without armor")
+{
     std::shared_ptr<Entity> entity = std::make_shared<Entity>("Entity", 100, 10, 0);
 
     entity->defend(10);
@@ -29,13 +32,15 @@ TEST_CASE("Test Entity defend method without armor") {
     REQUIRE(entity->getHealth() == 90);
 }
 
-TEST_CASE("Test Entity exists method when health is greater than 0") {
+TEST_CASE("Test Entity exists method when health is greater than 0")
+{
     std::shared_ptr<Entity> entity = std::make_shared<Entity>("Entity", 100, 10, 20);
 
     REQUIRE(entity->exists() == true);
 }
 
-TEST_CASE("Test Entity exists method when health is equal to 0") {
+TEST_CASE("Test Entity exists method when health is equal to 0")
+{
     std::shared_ptr<Entity> entity = std::make_shared<Entity>("Entity", 0, 10, 20);
 
     REQUIRE(entity->exists() == false);

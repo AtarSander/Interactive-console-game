4cd33143254bb895c4159c2822ded0c8b6b2e4ee
root <aszymczy>
2023-04-26 09:31:32 +0200

    Added randomizer functions, and default randomizing values

--- LAB4/entityfactory.cpp
+++ LAB4/entityfactory.cpp
@@ -6,6 +6,7 @@
 #include <memory>
 #include <string>
 #include <stdexcept>
+#include <random>
 
 std::shared_ptr<Entity> EntityFactory::createEntity(EntityType type, std::string name,
                                          int health, int base_damage,
@@ -13,6 +14,26 @@ std::shared_ptr<Entity> EntityFactory::createEntity(EntityType type, std::string
                                          std::string weapon_type,
                                          double crit_chance)
 {
+  if (health == 0)
+  {
+    health = randomizeInt(50, 100);
+  }
+  if (base_damage == 0)
+  {
+     base_damage = randomizeInt(5, 20);
+  }
+  if (base_armor == 0)
+  {
+     base_armor = randomizeInt(5, 20);
+  }
+  if (base_damage == 0)
+  {
+     base_damage = randomizeInt(5, 20);
+  }
+  if (crit_chance == 0)
+  {
+    crit_chance = randomizeFloat(0.2);
+  }
   switch (type)
   {
     case EntityType::Enemy:
@@ -30,6 +51,7 @@ std::shared_ptr<Entity> EntityFactory::createEnemy(std::string name, int health,
                                                    int base_armor, std::map<std::string,
                                                    double> enemy_resistances)
 {
+
   return std::make_shared<Enemy>(name, health, base_damage, base_armor, enemy_resistances);
 }
 
@@ -43,4 +65,24 @@ std::shared_ptr<Entity> EntityFactory::createPlayer(std::string name, int health
                                                     int base_armor)
 {
   return std::make_shared<Player>(name, health, base_damage, base_armor);
+}
+
+int randomizeInt(int border_down, int border_up)
+{
+    if (border_down > border_up)
+    {
+        return 0;
+    }
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(border_down, border_up);
+    return dis(gen);
+}
+
+double randomizeFloat(double border)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dis(0.0, border);
+    return dis(gen);
 }
